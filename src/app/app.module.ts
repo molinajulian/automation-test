42ea4e8d8aaa1732354163593dd280755d1524f4
Ivan D Cuadro <ivan.cuadro@wolox.co>
2019-08-01 13:42:43 -0500

    Login funcionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './screens/login/login.component';
+import { InputComponent } from './components/input/input.component';
+import { FormSubmitDirective } from './directives/form-submit.directive';
+import { CouponsComponent } from './screens/coupons/coupons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InputComponent,
+    FormSubmitDirective,
+    CouponsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
